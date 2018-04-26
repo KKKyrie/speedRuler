4363a18c96a85fb085276b9d7de24a6b24163cca
kyriejnliu <kyriejnliu@lexinfintech.com>
2018-04-26 10:43:50 +0800

    修复了无法监听onload事件的问题&若结果数组为空，重新获取

--- src/content_scripts/ruler.js
+++ src/content_scripts/ruler.js
@@ -18,11 +18,11 @@ const Page = {
 
 		let loadTime = (time.loadEventEnd - time.loadEventStart) / 1000;
 
-		if(loadTime < 0) {
-				setTimeout(function() {
-						_this.getTiming();
-				}, 200);
-				return;
+		if (loadTime < 0) {
+			setTimeout(function() {
+				_this.getTiming();
+			}, 200);
+			return;
 		}
 
 		timingObj['重定向时间'] = (time.redirectEnd - time.redirectStart) / 1000;
@@ -39,7 +39,7 @@ const Page = {
 
 		let timingArr = [];
 
-		for (let key in timingObj){
+		for (let key in timingObj) {
 			timingArr.push({
 				'name': key,
 				'durations(s)': timingObj[key]
@@ -54,18 +54,19 @@ const Page = {
 	getResourceTiming() {
 
 		if (!window.performance && !window.performance.getEntries) {
-        return false;
-    }
+			console.log('浏览器版本过低，不支持performance.');
+			return false;
+		}
 
-    var resourceArr = [];
-    window.performance.getEntries().forEach(function(perf){
-        resourceArr.push({
-            'url': perf.name,
-            'entryType': perf.entryType,
-            'type': perf.initiatorType,
-            'duration(ms)': perf.duration
-        });
-    });
+		var resourceArr = [];
+		window.performance.getEntries().forEach(function(perf) {
+			resourceArr.push({
+				'url': perf.name,
+				'entryType': perf.entryType,
+				'type': perf.initiatorType,
+				'duration(ms)': perf.duration
+			});
+		});
 
 		this.data.resourceArr = resourceArr;
 	},
@@ -77,7 +78,12 @@ const Page = {
 
 	bindEvent() {
 		let that = this;
+		
 		onMessage.addListener(function(req, sender, sendResponse) {
+			if (that.data.timingArr.length === 0 || that.data.resourceArr.length === 0) {
+				console.log(':)');
+				that.initData();
+			}
 
 			if (req.action === 'MEASURE') {
 				console.table(that.data.timingArr);
@@ -91,7 +97,7 @@ const Page = {
 			}
 		});
 
-		window.addEventListener('load', function(){
+		window.addEventListener('load', function() {
 			that.initData();
 		}, false);
 	},
